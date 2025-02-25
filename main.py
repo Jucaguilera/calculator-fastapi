ea34ba8b9772f34e952a9fb26227b9dbd1619d9b
Juca Cardoso <jucacrvg@gmail.com>
2025-02-25 18:08:01 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"mensagem": "Bem-vindo à API da Calculadora"}
+
+@app.get("/calcular")
+def calcular(operacao: str, a: float, b: float):
+    if operacao == "soma":
+        return {"resultado": a + b}
+    elif operacao == "subtracao":
+        return {"resultado": a - b}
+    elif operacao == "multiplicacao":
+        return {"resultado": a * b}
+    elif operacao == "divisao":
+        if b == 0:
+            return {"erro": "Não é possível dividir por zero"}
+        return {"resultado": a / b}
+    else:
+        return {"erro": "Operação inválida! Use soma, subtracao, multiplicacao ou divisao"}
---
+++ meu.scrip.py
@@ -0,0 +1,4 @@
+print("Olá, Juca! Seu Python está funcionando no VS Code!")
+python -m venv venv
+pip install fastapi uvicorn
+
